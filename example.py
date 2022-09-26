48189c7ff288cac5147b6fb77975de2544733420
Peerachet Porkaew <peerachet.porkaew@gmail.com>
2022-09-26 15:41:14 +0700

    update

--- example.py
+++ example.py
@@ -1,22 +1,24 @@
-#from pythainlp.tokenize import subword_tokenize
+from pythainlp.tokenize import subword_tokenize
 import re 
 
 
 text = ("พระราชบัญญัติธรรมนูญการปกครองแผ่นดินสยามชั่วคราว พุทธศักราช ๒๔๗๕ "
         "เป็นรัฐธรรมนูญฉบับชั่วคราว ซึ่งถือว่าเป็นรัฐธรรมนูญฉบับแรกแห่งราชอาณาจักรสยาม "
-        "ประกาศใช้เมื่อวันที่ 27 มิถุนายน พ.ศ. 2475 Mancity 20:43 "
-        "โดยเป็นผลพวงหลังการปฏิวัติเมื่อวันที่ 24 มิถุนายน พ.ศ. 2475 โดยคณะราษฎร")
+        "ประกาศใช้เมื่อวันที่ 27 มิถุนายน พ.ศ. 2475 Mancity Co.,LTD 20:43 12_2 นายประยุทธ์ จันทร์โอชา"
+        "โดยเป็นผลพวงหลังการปฏิวัติเมื่อวันที่ 24 มิถุนายน พ.ศ. 2475 โดยคณะราษฎร 3,4,5 ฉันเป็นหวัด 12-000 บาท")
 
 out = subword_tokenize(text,engine="dict",keep_whitespace=True)
 print(out)
 
+out = "|".join(out)
+
 
 def convert_num(match_obj):
-    print("MATCH",match_obj.group(0))
     if match_obj.group(0) is not None:
         return match_obj.group(0).replace("|","") + "|"
 
-text = "a|b|c|1|.|2|2|e|1|2|"
-numRegex = re.sub(r'([\d.]\|)+',convert_num,text)
-print(numRegex)
+text = out
+numRegex = re.sub(r'([\d]\|)+',convert_num,text)
 
+output = numRegex.replace("_","[!und:]").replace(" ","_")
+print(output.split("|"))
